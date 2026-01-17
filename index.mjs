a3f46faef152280b43e098b64bdbb92888cff60e
Daniil <benerd.develop@gmail.com>
2026-01-17 22:22:15 +0700

    feat: added routes for getting info about song and for searching songs

--- index.mjs
+++ index.mjs
@@ -15,7 +15,57 @@ app.get('/health', async (req, res) => {
         res.json({ error: e.message })
     }
 })
+app.get('/song/search', async (req, res) => {
+    try {
+        const { q } = req.query
+
+        const response = await fetch(`https://api.deezer.com/search?q=${q}`, {
+            headers: {
+                'Accept-Language': 'en',
+            },
+        })
+
+        const songs = await response.json()
+
+        res.json({
+            results: songs.data.map((song) => ({
+                id: song.id,
+                title: song.title,
+                artist: song.artist.name,
+                preview: song.preview,
+                image: song.album.cover_big,
+            })),
+        })
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({ error: e.message })
+    }
+})
+
+app.get('/song/:trackId', async (req, res) => {
+    try {
+        const { trackId } = req.params
+
+        const response = await fetch(`https://api.deezer.com/track/${trackId}`, {
+            headers: {
+                'Accept-Language': 'en',
+            },
+        })
+
+        const song = await response.json()
+
+        res.json({
+            id: song.id,
+            title: song.title,
+            preview: song.preview,
+            image: song.album.cover_big,
+        })
+    } catch (e) {
+        res.status(500).json({ error: e.message })
+    }
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
+    console.log('http://localhost:3000')
 })
